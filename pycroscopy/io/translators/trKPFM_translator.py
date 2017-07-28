f3291b40cfe15a808dd2f901a4206be07d56eda1
ramav87 <rama87@gmail.com>
2017-07-28 18:03:41 -0400

    time-resolved KPFM translator

--- pycroscopy/io/translators/trKPFM_translator.py
+++ pycroscopy/io/translators/trKPFM_translator.py
@@ -104,10 +104,10 @@ class TRKPFMTranslator(Translator):
 
         f = open(self.file_list[0], 'rb')
         spectrogram_size, count_vals = self._parse_spectrogram_size(f)
-        print("spectrogram size:", spectrogram_size)
+        #print("spectrogram size:", spectrogram_size)
         num_pixels = parm_dict['grid_num_rows']*parm_dict['grid_num_cols']
-        print('Number of pixels: ', num_pixels)
-        print('Count Values: ', count_vals)
+        #print('Number of pixels: ', num_pixels)
+        #print('Count Values: ', count_vals)
         if (num_pixels+1) != count_vals:
             print("Data size does not match number of pixels expected. Cannot continue")
 
@@ -271,7 +271,7 @@ class TRKPFMTranslator(Translator):
         parm_dict = dict()
 
         parm_dict['IO_samp_rate_[Hz]'] = np.uint32(h5_f['IO_rate'][0][0])
-        parm_dict['IO_time'] = np.uint32(h5_f['IO_time'][0][0])
+        parm_dict['IO_time'] = np.float32(h5_f['IO_time'][0][0])
 
         excit_wfm = np.float32(np.squeeze(h5_f['dc_amp_vec']))
 

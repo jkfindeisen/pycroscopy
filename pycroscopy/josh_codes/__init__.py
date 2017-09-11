2e1a36102db0e4561fda54919b6810f12d02e691
ssomnath <somnaths@ornl.gov>
2017-09-11 15:29:35 -0400

    first commit to branch

---
+++ pycroscopy/josh_codes/__init__.py
@@ -0,0 +1,14 @@
+"""
+Pycroscopy's processing module
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+    plotting_format
+
+"""
+from . import plotting_format
+from .plotting_format import *

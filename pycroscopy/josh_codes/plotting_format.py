b7b0f382647ac67420d5d07bcd9d7acda86544ea
jagar2 <jagar@berkeley.edu>
2017-09-14 17:27:29 -0700

    Add conduct_PCA

--- pycroscopy/josh_codes/plotting_format.py
+++ pycroscopy/josh_codes/plotting_format.py
@@ -1,2 +1,37 @@
 def josh_test(name):
     print(f'you typed in {name}, the test workeddd')
+
+
+def conduct_PCA(loops, n_components=15):
+
+    """
+    Computes the PCA and forms a low-rank representation of a series of response curves
+    This code can be applied to all forms of response curves.
+    loops = [number of samples, response spectra for each sample]
+
+    Parameters
+    ----------
+    loops : numpy array
+        2d numpy array - [number of samples, response spectra for each sample]
+    n_components : int, optional
+        int - sets the number of componets to save
+
+    Returns
+    -------
+    PCA : object
+        results from the PCA
+    PCA_reconstructed : numpy array
+        low-rank representation of the raw data reconstructed based on PCA denoising
+    """
+
+    # Sets the number of components to save
+    pca = decomposition.PCA(n_components=n_components)
+
+    # Computes the PCA of the piezoelectric hysteresis loops
+    PCA = pca.fit(loops)
+
+    # does the inverse transform - creates a low rank representation of the data
+    # this process denoises the data
+    PCA_reconstructed= pca.inverse_transform(pca.transform(loops))
+
+    return(PCA, PCA_reconstructed)

2d9b497c555c657fe16724e1662a61fecce8c5ac
jagar2 <jagar@berkeley.edu>
2017-09-14 18:03:42 -0700

    add resizing and verbosity

--- pycroscopy/josh_codes/plotting_format.py
+++ pycroscopy/josh_codes/plotting_format.py
@@ -1,7 +1,7 @@
 from sklearn import (decomposition)
 
-def conduct_PCA(loops, n_components=15):
 
+def conduct_PCA(loops, n_components=15, verbose=True):
     """
     Computes the PCA and forms a low-rank representation of a series of response curves
     This code can be applied to all forms of response curves.
@@ -10,9 +10,11 @@ def conduct_PCA(loops, n_components=15):
     Parameters
     ----------
     loops : numpy array
-        2d numpy array - [number of samples, response spectra for each sample]
+        1 or 2d numpy array - [number of samples, response spectra for each sample]
     n_components : int, optional
         int - sets the number of componets to save
+    verbose : bool, optional
+        output operational comments
 
     Returns
     -------
@@ -22,6 +24,15 @@ def conduct_PCA(loops, n_components=15):
         low-rank representation of the raw data reconstructed based on PCA denoising
     """
 
+    # resizes the array for hyperspectral data
+    if loops.size == 3:
+        original_size = loops.shape[0]
+        loops.reshape(-1, loops.shape[2])
+        verbose_print(verbose, f'shape of data resized to [{loop.shape[0]} x {loop.shape[1]}]')
+    elif loop.size == 2:
+    else:
+        raise ValueError("data is of an incorrect size")
+
     # Sets the number of components to save
     pca = decomposition.PCA(n_components=n_components)
 
@@ -30,6 +41,16 @@ def conduct_PCA(loops, n_components=15):
 
     # does the inverse transform - creates a low rank representation of the data
     # this process denoises the data
-    PCA_reconstructed= pca.inverse_transform(pca.transform(loops))
+    PCA_reconstructed = pca.inverse_transform(pca.transform(loops))
+
+    # resized the array for hyperspectral data
+    if loops.size == 3:
+        PCA.reshape(original_size, original_size, -1)
+        PCA_reconstructed.reshape(original_size, original_size, -1)
 
     return(PCA, PCA_reconstructed)
+
+
+def verbose_print(verbose, *args):
+    if verbose:
+        print(*args)

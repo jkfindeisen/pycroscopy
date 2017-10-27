af1bb0a6a98fc91b423472c12e7a312e74f6ad68
jagar2 <jagar@berkeley.edu>
2017-10-27 11:59:55 -0700

    asdas

--- pycroscopy/josh_codes/__init__.py
+++ pycroscopy/josh_codes/__init__.py
@@ -12,13 +12,6 @@ Submodules
 """
 from . import plotting_format
 from .plotting_format import *
-<<<<<<< HEAD
-
 
 from . import cKPFM
 from .cKPFM import *
-
-from . import Autoencoder
-from .Autoencoder import *
-=======
->>>>>>> first commit to branch
